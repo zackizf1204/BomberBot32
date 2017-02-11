86e8ad9393dfce12e216a0ef82b357a4589a56c6
zackizf1204 <zackizf1204@gmail.com>
2017-02-11 23:35:42 +0700

    Update tipe.h

--- BomberBot32/BomberBot32/tipe.h
+++ BomberBot32/BomberBot32/tipe.h
@@ -1,8 +1,10 @@
 
 #ifndef Tipe_H
 #define Tipe_H
+
 #include "point.h"
 #include "boolean.h"
+
 /*  Kamus Umum */
 #define IdxMax 100
 /* Indeks maksimum array, sekaligus ukuran maksimum array dalam C */
@@ -25,11 +27,11 @@ typedef struct
 {
 	char Name[25];
 	char Key;
-	int Point;
+	int Points;
 	bool Status;
 	Bombs B;
 	int BlastRadius;
-	POINT P;
+	POINT Location;
 } Player;
 
 //Type Data Map
@@ -49,5 +51,34 @@ typedef struct {
 	int Neff; /* >=0, banyaknya elemen efektif */
 } TabPlayer;
 
+/* Setter dan Getter untuk ADT Bombs*/
+
+#define BombLoc(C)	(C).Location
+#define Fuse(C)		(C).Fuse
+#define Radius(C)	(C).Radius
+
+/* Setter dan Getter untuk ADT Player*/
+
+#define Name(C)			(C).Name
+#define Key(C)			(C).Key
+#define Points(C)		(C).Points
+#define Status(C)		(C).Status
+#define Bombs(C)		(C).B
+#define Blast(C)		(C).BlastRadius
+#define PlayerLoc(C)	(C).Location
+
+/* Setter dan Getter untuk ADT Map*/
+
+#define Width(C)	(C).Width
+#define Height(C)	(C).Height
+#define Round(C)	(C).Round
+#define Seed(C)		(C).Seed
+#define Bounty(C)	(C).Bounty
+
+/* Setter dan Getter untuk ADT TabPlayer*/
+
+#define Neff(T)   (T).Neff
+#define TI(T)     (T).TI
+#define Elmt(T,i) (T).TI[i]
 
 #endif
